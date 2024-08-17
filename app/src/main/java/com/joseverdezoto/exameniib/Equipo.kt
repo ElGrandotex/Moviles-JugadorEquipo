49fa0c971c2de602f3dd98d0e0cd4dc4c3428eb3
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-08-17 12:32:31 -0500

    Data class

---
+++ app/src/main/java/com/joseverdezoto/exameniib/Equipo.kt
@@ -0,0 +1,9 @@
+package com.joseverdezoto.exameniib
+
+data class Equipo(
+    val id: Int,
+    val nombre: String,
+    val ciudad: String,
+    val entrenador: String,
+    val liga: String
+)
---
+++ app/src/main/java/com/joseverdezoto/exameniib/Jugador.kt
@@ -0,0 +1,9 @@
+package com.joseverdezoto.exameniib
+
+data class Jugador(
+    val id: Int,
+    val nombre: String,
+    val posicion: String,
+    val dorsal: Int,
+    val equipo: Int
+)
