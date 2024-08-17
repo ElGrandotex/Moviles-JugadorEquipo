23b426ea3461b26c8ca1385cf732368fd5ddd1be
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-08-17 12:34:43 -0500

    Main

--- app/src/main/java/com/joseverdezoto/exameniib/MainActivity.kt
+++ app/src/main/java/com/joseverdezoto/exameniib/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.joseverdezoto.exameniib
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.joseverdezoto.exameniib.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var databaseHelper: JugadorDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = JugadorDatabaseHelper(this)
+
+        val jugadorAdapter = JugadorAdapter(databaseHelper.getJugadores(), this)
+        binding.jugadorRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.jugadorRecyclerView.adapter = jugadorAdapter
+
+        binding.addJugadorButton.setOnClickListener {
+            val intent = Intent(this, AddJugadorActivity::class.java)
+            startActivity(intent)
         }
+
+        val equipoAdapter = EquipoAdapter(databaseHelper.getEquipos(), this)
+        binding.equipoRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.equipoRecyclerView.adapter = equipoAdapter
+
+        binding.addEquipoButton.setOnClickListener {
+            val intent = Intent(this, AddEquipoActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+    override fun onResume() {
+        super.onResume()
+        val jugadorAdapter = JugadorAdapter(databaseHelper.getJugadores(), this)
+        binding.jugadorRecyclerView.adapter = jugadorAdapter
+        val equipoAdapter = EquipoAdapter(databaseHelper.getEquipos(), this)
+        binding.equipoRecyclerView.adapter = equipoAdapter
     }
 }
